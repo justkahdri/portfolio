886cfa7c5359bf76f12c20185140fdcec5294c39
Joaquín R. Montes <joaco.rm512@gmail.com>
2021-02-10 14:59:28 -0300

    footer finished

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,20 +4,25 @@ import './styles/footer.css';
 
 const Footer = () => {
     return (
-        <footer id="footer" className="pb-4 pt-4 bg-dark">
+        <footer id="footer" className="bg-dark py-2">
             <div className="container">
-                <div className="row text-center">
-                    <div className="col-md-4 col-lg">
-                        <button className="btn btn-primary">
-                            Github
-                        </button>
+                <div className="row align-items-center">
+                    <div className="col-md-4 col-lg text-center">
+                        <a href='https://cafecito.app/justkahdri' rel='noopener noreferrer' target='_blank'><img
+                            srcSet='https://cdn.cafecito.app/imgs/buttons/button_1.png 1x, https://cdn.cafecito.app/imgs/buttons/button_1_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_1_3.75x.png 3.75x'
+                            src='https://cdn.cafecito.app/imgs/buttons/button_1.png'
+                            alt='Invitame un café en cafecito.app'/></a>
                     </div>
-                    <div className="col-md-4 col-lg">
-                        <button className="btn btn-link">About Me</button>
+                    <div className="col-md-4 col-lg text-center">
+                        <h6 className="bold text-light mb-0">
+                            JustKahdri © {new Date().getFullYear() || 2021}
+                        </h6>
                     </div>
-                    <div className="col-md-4 col-lg align-self-end text-center">
+                    {document.documentElement.offsetHeight === window.innerHeight ||
+                    <div className="col-md-4 col-lg text-center">
                         <a href="#" className="to-top">Back to top</a>
                     </div>
+                    }
                 </div>
 
             </div>
